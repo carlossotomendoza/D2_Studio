4afd8413d8a8d291c66a86577918b015fa2e161a
Carlos Soto <carlos.soto@stark.cloud>
2024-06-25 16:41:32 -0400

    Cambios en paquetes

---
+++ DevClassic/Schemas/UsrRealtyClassic1Page/UsrRealtyClassic1Page.js
@@ -0,0 +1,206 @@
+define("UsrRealtyClassic1Page", [], function() {
+	return {
+		entitySchemaName: "UsrRealtyClassic",
+		attributes: {},
+		modules: /**SCHEMA_MODULES*/{}/**SCHEMA_MODULES*/,
+		details: /**SCHEMA_DETAILS*/{
+			"Files": {
+				"schemaName": "FileDetailV2",
+				"entitySchemaName": "UsrRealtyClassicFile",
+				"filter": {
+					"masterColumn": "Id",
+					"detailColumn": "UsrRealtyClassic"
+				}
+			}
+		}/**SCHEMA_DETAILS*/,
+		businessRules: /**SCHEMA_BUSINESS_RULES*/{}/**SCHEMA_BUSINESS_RULES*/,
+		methods: {},
+		dataModels: /**SCHEMA_DATA_MODELS*/{}/**SCHEMA_DATA_MODELS*/,
+		diff: /**SCHEMA_DIFF*/[
+			{
+				"operation": "insert",
+				"name": "UsrName32942ba2-62c3-45ba-9d43-73d1902771a1",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 0,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrName",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "FLOAT1d9b2406-6e5d-4522-841d-a437bab016ca",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 1,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrPriceUSD",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "FLOAT4d078f87-12e5-4240-845b-4575cb960b60",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 2,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrArea",
+					"enabled": true
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "LOOKUPe30bfb01-d462-4491-973b-bd22deef9cb1",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 0,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrType",
+					"enabled": true,
+					"contentType": 3
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "LOOKUPb4063439-be9e-4561-a23d-10fcb84bf4e1",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 12,
+						"row": 0,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrOfferType",
+					"enabled": true,
+					"contentType": 3
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "STRING80d1bb0e-aced-484f-8215-6d5f6aa74cb8",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 2,
+						"column": 0,
+						"row": 1,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrComment",
+					"enabled": true,
+					"contentType": 0
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "NotesAndFilesTab",
+				"values": {
+					"caption": {
+						"bindTo": "Resources.Strings.NotesAndFilesTabCaption"
+					},
+					"items": [],
+					"order": 0
+				},
+				"parentName": "Tabs",
+				"propertyName": "tabs",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "Files",
+				"values": {
+					"itemType": 2
+				},
+				"parentName": "NotesAndFilesTab",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "NotesControlGroup",
+				"values": {
+					"itemType": 15,
+					"caption": {
+						"bindTo": "Resources.Strings.NotesGroupCaption"
+					},
+					"items": []
+				},
+				"parentName": "NotesAndFilesTab",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "Notes",
+				"values": {
+					"bindTo": "UsrNotes",
+					"dataValueType": 1,
+					"contentType": 4,
+					"layout": {
+						"column": 0,
+						"row": 0,
+						"colSpan": 24
+					},
+					"labelConfig": {
+						"visible": false
+					},
+					"controlConfig": {
+						"imageLoaded": {
+							"bindTo": "insertImagesToNotes"
+						},
+						"images": {
+							"bindTo": "NotesImagesCollection"
+						}
+					}
+				},
+				"parentName": "NotesControlGroup",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "merge",
+				"name": "ESNTab",
+				"values": {
+					"order": 1
+				}
+			}
+		]/**SCHEMA_DIFF*/
+	};
+});
